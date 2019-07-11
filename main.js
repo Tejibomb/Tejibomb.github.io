e97357ef3bc624815cd24af6a01beabbe0c40a7e
tejibomb <tejiriewherido@gmail.com>
2019-07-11 08:48:52 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+alert("All right nerds!!! Let's Go!!!!!");
+
+
+function WhosNo1()
+{
+  document.getElementById('cas').src='REF.jpg' ;
+}
+function WhosNo2()
+{
+  document.getElementById('cas').src='BakuJapUASheet.png' ;
+   x = 2
+}
+
+function translate()
+{
+  document.getElementById('cas').src='BakuEngUASheet.jpg' ;
+}
+function WhosNo4()
+{
+  document.getElementById('cas').src='Katsuki_Bakugo_Civilian_Profile.png' ;
+}
